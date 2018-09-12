5b5329274eb29ad9a13085e9e3b3fa21b9e7a9a8
Alexander Schelchere <ascellie@gmail.com>
2018-09-12 18:53:55 -0400

    Word Count

---
+++ WordCount/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+
+namespace WordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("An argument is expected.");
+                return;
+            }
+            if (!File.Exists(args[0]))
+            {
+                Console.WriteLine($"File not found: {args[0]}");
+            }
+            string text = File.ReadAllText(args[0]);
+            const string word = "whale";
+            int position = text.IndexOf(word);
+            int count = 0;
+            while (position >= 0)
+            {
+                count++;
+                position = text.IndexOf(word, position + 1);
+            }
+            Console.WriteLine($"The word \"{word}\" was found {count} times.");
+        }
+    }
+}
