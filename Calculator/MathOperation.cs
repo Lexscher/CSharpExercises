f416bfa2f5fb5657c7c64b1066d62c95e276cb91
Alexander Schelchere <ascellie@gmail.com>
2018-09-13 10:22:16 -0400

    Calculator

---
+++ Calculator/MathOperation.cs
@@ -0,0 +1,48 @@
+using System;
+namespace Calculator
+{
+    public class MathOperation
+    {
+        const string Operators = "+-*/%";
+        public static MathOperation ReadFromConsole()
+        {
+            Console.Write("Enter the left operand: ");
+            string sLeft = Console.ReadLine();
+            if (!double.TryParse(sLeft, out double leftOp)) return null;
+            Console.Write("Enter the right operand: ");
+            if (!double.TryParse(Console.ReadLine(), out double rightOp)) return null;
+            Console.Write($"Enter an operator ({Operators}): ");
+            char c = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            if (!Operators.Contains(c)) return null;
+            return new MathOperation
+            {
+                LeftOperand = leftOp,
+                RightOperand = rightOp,
+                Operator = c
+            };
+        }
+        public double LeftOperand { get; private set; }
+        public double RightOperand { get; private set; }
+        public char Operator { get; private set; }
+        public double Result
+        {
+            get
+            {
+                switch (Operator)
+                {
+                    case '+': return LeftOperand + RightOperand;
+                    case '-': return LeftOperand - RightOperand;
+                    case '*': return LeftOperand * RightOperand;
+                    case '/': return LeftOperand / RightOperand;
+                    case '%': return LeftOperand % RightOperand;
+                    default: throw new Exception($"Unexpected Operator: {Operator}");
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return $"{LeftOperand} {Operator} {RightOperand} = {Result}";
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperation mathOp = MathOperation.ReadFromConsole();
+            while(mathOp != null)
+            {
+                Console.WriteLine(mathOp);
+                Console.WriteLine();
+                mathOp = MathOperation.ReadFromConsole();
+            }
+        }
+    }
+}
